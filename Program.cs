417216816cf3cea64a4c14f03a2adbce837d3641
unknown <tocialdo@gmail.com>
2024-07-01 17:01:58 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.API.Services;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Logger;
+using System;
+using System.Threading.Tasks;
+
+namespace InstagramApiProject
+{
+    class Program
+    {   
+        static async Task Main(string[] args)
+        {
+            // Use RegistrationService
+            // var registrationService = API
+            IInstaApi api = InstaApiBuilder.CreateBuilder()
+                // .SetUser(new UserSessionData
+                // {
+                //     UserName = "USERNAME",
+                //     Password = "PASSWORD"
+                // })
+                .UseLogger(new DebugLogger(LogLevel.Exceptions))
+                .Build();
+        
+            api.RegistrationService.CreateNewAccountWithEmailAsync(
+                "brafrossappacro-7714@yopmail.com",
+                "brafrossappacro7714",
+                "BRfro$$@CPR",
+                "brafross appacro").Wait();
+        }
+    }
+}
